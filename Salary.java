4cf0890410536cb9b8b2157bcfe5fd3ecac61ea1
annaannmathew <85623708+annaannmathew@users.noreply.github.com>
2022-01-28 01:12:54 +0530

    Add files via upload

---
+++ Salary.java
@@ -0,0 +1,29 @@
+class Employee
+{
+    void display()
+    {
+        System.out.println("Name of the class is Employee.");
+    }
+    void calcSalary()
+    {
+        System.out.println("Salary of employee is 10000");
+    }
+}
+
+class Engineer extends Employee
+{
+    void calcSalary()
+    {
+        System.out.println("Salary of employee is 20000.");
+    }
+}
+
+public class Salary
+{
+    public static void main(String args[])
+    {
+        Engineer obj=new Engineer();
+        obj.display();
+        obj.calcSalary();
+    }
+}
