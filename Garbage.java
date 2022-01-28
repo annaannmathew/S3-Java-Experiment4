9daefef8de07aca9ec4fe27deb854474da5f49bc
annaannmathew <85623708+annaannmathew@users.noreply.github.com>
2022-01-28 23:35:25 +0530

    Add files via upload

---
+++ Garbage.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+public class Garbage
+{  
+    public void finalize()
+    {
+        System.out.println("object is garbage collected");
+    } 
+    public static void main(String args[])
+    {  
+        Garbage s1=new Garbage();  
+        Garbage s2=new Garbage();
+        s1=null;
+        s2=null;
+        System.gc();
+    }  
+} 
